dce516aa282fb714c205a96198e8170b21c78ade
Hakan Yildiz <hakan@re-coded.com>
2020-04-06 11:00:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
 
 function findMatching(drivers, string){
   
-  
+  let newDriver = drivers.filter(function(arr){
+    return arr === string;
+  })
 }
